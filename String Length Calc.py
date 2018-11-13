37e107b0204e9e982d6876cf6e66587df20c3394
Tem <33301256+TemCodes@users.noreply.github.com>
2018-11-13 02:05:59 -0800

    Update String Length Calc.py

--- String Length Calc.py
+++ String Length Calc.py
@@ -2,7 +2,7 @@ def string_leng(s1):
     count = 0
     for letter in s1:
         count += 1
-    print("Length of string is:", count)
+    print("Length of string is: ", count)
 
 
 string_leng('ererreg')
